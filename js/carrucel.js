caf2b42cdd52bf5ba2063518c1a63874c5c550a5
svanessar <104364530+svanessar@users.noreply.github.com>
2022-09-30 10:37:12 -0500

    Add files via upload

---
+++ js/carrucel.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const cajas    = document.querySelector('.cajas')
+const punto     = document.querySelectorAll('.punto')
+
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX alas cajas
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+// Recorrer TODOS los punto
+punto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -33
+
+        // MOVEMOS el grand
+        cajas.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    })
+})
